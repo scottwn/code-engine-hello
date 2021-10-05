051a366fa423307b588d68f7b936a6cab0a9896f
Scott Wyman Neagle <wymnea@protonmail.com>
2021-10-05 15:21:55 -0500

    get_design_name

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from qiskit_metal import designs
 app = Flask(__name__)
 
 @app.route('/say_hello')
@@ -8,3 +9,8 @@ def say_hello():
 @app.route('/say_hello_again')
 def say_hello_again():
     return 'hello world again'
+
+@app.route('/get_design_name')
+def get_design_name():
+    design = designs.DesignPlanar()
+    return design.get_design_name()
